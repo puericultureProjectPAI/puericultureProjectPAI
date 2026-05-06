a1b4dad5e168fe7f7ce2ee5f1b72cf270e5a0c4e
Xavier Moyon <moyonxavier@gmail.com>
2026-05-07 01:37:24 +0200

    refactor(COMMON): Ajout de commentaires et renommage de la classe d'erreur interne ref: #19

--- project/back/src/main/java/com/puericulture/forwardtrading/controller/ForwardTradingController.java
+++ project/back/src/main/java/com/puericulture/forwardtrading/controller/ForwardTradingController.java
@@ -1,24 +1,9 @@
 package com.puericulture.forwardtrading.controller;
 
-import com.puericulture.config.errormanager.exception.*;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 @RequestMapping("/forward-trading")
-public class ForwardTradingController {
-    @GetMapping()
-    public Object testMapping(@RequestParam Integer errorCode){
-        switch (errorCode){
-            case 500: throw new InternalServorError();
-            case 400: throw new BadRequestException();
-            case 401: throw new UnauthorizedException();
-            case 403: throw new ForbiddenException();
-            case 404: throw new NotFoundException();
-            case 501: throw new NotImplementedException();
-            default: return new Object();
-        }
-    }
+public abstract class ForwardTradingController {
 }
