589514c5e43248ec24af6326f974eefab83253c2
Leo.defossez <leo.defossez.etu@univ-lille.fr>
2026-05-09 00:30:28 +0200

    fix: the CD now always generate new versions of the app

--- project/back/src/main/java/com/puericulture/App.java
+++ project/back/src/main/java/com/puericulture/App.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-// desactivate automatic security config to start.
+// desactivate automatic security config to start
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class App {
     public static void main(String[] args) {
