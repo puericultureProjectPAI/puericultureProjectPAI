658d9bda19e4ec4b0a71834861b4af6d032908e1
Xavier Moyon <moyonxavier@gmail.com>
2026-05-13 20:43:48 +0200

    refactor: move MyTextInput into form folder. Change register button by a NavLink and change register form style

--- project/front/src/common/components/RegisterForm.jsx
+++ project/front/src/common/components/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import { Formik, Form } from "formik";
+import { NavLink } from "react-router";
 import * as Yup from "yup";
-import MyTextInput from "./MyTextInput";
+import MyTextInput from "./form/MyTextInput";
 
 const validationSchema = Yup.object({
   lastName: Yup.string().required("Le nom est requis."),
@@ -23,28 +24,30 @@ const validationSchema = Yup.object({
 
 export default function RegisterForm({ onSubmit, isLoading }) {
   return (
-    <Formik
-      initialValues={{
-        lastName: "",
-        firstName: "",
-        birthDate: "",
-        email: "",
-        password: "",
-      }}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      <Form>
-        <MyTextInput label="Nom" name="lastName" type="text" />
-        <MyTextInput label="Prénom" name="firstName" type="text" />
-        <MyTextInput label="Date de naissance" name="birthDate" type="date" />
-        <MyTextInput label="Email" name="email" type="email" />
-        <MyTextInput label="Mot de passe" name="password" type="password" />
-
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? "Inscription..." : "S'inscrire"}
-        </button>
-      </Form>
-    </Formik>
+    <>
+      <Formik
+        initialValues={{
+          lastName: "",
+          firstName: "",
+          birthDate: "",
+          email: "",
+          password: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form class="flex flex-col">
+          <MyTextInput label="Nom" name="lastName" type="text" />
+          <MyTextInput label="Prénom" name="firstName" type="text" />
+          <MyTextInput label="Date de naissance" name="birthDate" type="date" />
+          <MyTextInput label="Email" name="email" type="email" />
+          <MyTextInput label="Mot de passe" name="password" type="password" />
+          <button type="submit" disabled={isLoading}>
+            {isLoading ? "Inscription..." : "S'inscrire"}
+          </button>
+        </Form>
+      </Formik>
+      <NavLink to="../login">Login</NavLink>
+    </>
   );
 }
