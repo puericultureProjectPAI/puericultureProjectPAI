9355dcb386b2f7c6ebfc7651f68af056dcfa9e1c
Xavier Moyon <moyonxavier@gmail.com>
2026-05-18 23:57:23 +0200

    refactor(FT - Timeline generation): Add some comments for entity declaration

--- project/back/src/main/java/com/puericulture/forwardtrading/entity/TimelineEvents.java
+++ project/back/src/main/java/com/puericulture/forwardtrading/entity/TimelineEvents.java
@@ -16,18 +16,23 @@ public class TimelineEvents {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /** Event type : Example : BUY / SOLD */
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    /** Article name - Example : plush bob razowski */
     @Column(name = "article_name", nullable = false)
     private String articleName;
 
-    /**
-     * NUMERIC(10,2) mapping. We use BigDecimal to avoid floating-point inaccuracies with currency.
-     */
+    /** Estimated price */
     @Column(name = "article_price", precision = 10, scale = 2)
     private BigDecimal articlePrice;
 
+    /** Article category - Examples : - FOOD - TOY */
     @Column(name = "article_tag", length = 50)
     private String articleTag;
 
+    /** Period - to indicate to which period you should buy the article */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "period_id")
     private TimelinePeriods period;
--- project/back/src/main/java/com/puericulture/forwardtrading/entity/TimelinePeriods.java
+++ project/back/src/main/java/com/puericulture/forwardtrading/entity/TimelinePeriods.java
@@ -16,15 +16,22 @@ public class TimelinePeriods {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /** Period type - Example : Q1 (Quarter), First-Weeks */
     @Column(name = "type", length = 50)
     private String type;
 
+    /** Period name - Examples : "0-3 Months" , "0-2 Weeks" */
     @Column(name = "label", length = 50, nullable = false)
     private String label;
 
+    /** Period duration in week : Example : 4 */
     @Column(name = "week_duration", nullable = false)
     private Integer weekDuration;
 
+    /**
+     * Position in the timeline. Example : - Period Q1-Q3 is before Q4-Q7, so Q1-Q3's orderIndex is
+     * lower than Q4-Q7
+     */
     @Column(name = "order_index", nullable = false)
     private Integer orderIndex;
 
