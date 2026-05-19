2d1affcc4a35b510e5dffcedb9fb8fbd008c8086
julien <julien.miroux.etu@univ-lille.fr>
2026-05-20 01:18:51 +0200

    feat: add TanStack QueryClientProvider to main application entry

--- project/front/src/main.jsx
+++ project/front/src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./index.css";
 
 // Virtual import from vite-plugin-pwa
 import { registerSW } from "virtual:pwa-register";
-
+const queryClient = new QueryClient();
 // Initialize the Service Worker and listen for new deployments
 const updateSW = registerSW({
   onNeedRefresh() {
@@ -26,8 +27,10 @@ const updateSW = registerSW({
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
