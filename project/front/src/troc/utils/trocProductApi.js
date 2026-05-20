bb67c8a3b0d486c0bc3ba79ee696a4c17207ed65
“Your <brunineeinstein.pointe-jour@energir.com>
2026-05-20 18:35:05 -0500

    chore(TROC-2): supprimer les fichiers TrocProduct appartenant à Jingyi (TROC-1)

--- project/front/src/troc/utils/trocProductApi.js
+++
@@ -1,9 +0,0 @@
-import { apiClient } from "../../common/utils/apiClient";
-
-export async function createTrocProduct(data) {
-  return apiClient.post("/api/troc/products", data);
-}
-
-export async function getTrocProduct(id) {
-  return apiClient.get(`/api/troc/products/${id}`);
-}
