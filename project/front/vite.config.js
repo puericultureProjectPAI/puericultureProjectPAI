ae60b1cf144c99fd970659faf1e29067758f09a3
Xavier Moyon <moyonxavier@gmail.com>
2026-05-20 23:00:48 +0200

    fix(Cors config): add a proxy in front project and add /api for all backend endpoint

--- project/front/vite.config.js
+++ project/front/vite.config.js
@@ -51,4 +51,12 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
