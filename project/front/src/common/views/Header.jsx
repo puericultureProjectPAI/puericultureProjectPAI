ba1c5dca77eaa5d44daab645997f061186d3517f
Delcambre Jean-Alexis <jean-alexis.delcambre.etu@univ-lille.fr>
2026-05-21 12:10:21 +0200

    Remove a looping call

--- project/front/src/common/views/Header.jsx
+++ project/front/src/common/views/Header.jsx
@@ -1,11 +1,12 @@
 export default function Header() {
+  //A adapter plus tard avec la version PC
   return (
-    <Header className="flex h-14 items-center justify-between bg-[#080036] px-4 text-white">
+    <header className="flex h-14 items-center justify-between bg-[#080036] px-4 text-white">
       <div className="text-2xl font-extrabold tracking-wide">KIABI</div>
       <div className="flex items-center gap-3 text-xl" aria-hidden="true">
         <span>⌘</span>
         <span>♡</span>
       </div>
-    </Header>
+    </header>
   );
 }
