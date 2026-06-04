d8900ec1f1653d26f22062dd242700afc0ba91ad
julien <julien.miroux.etu@univ-lille.fr>
2026-06-05 00:36:41 +0200

    feat: create form onboarding with sucessScreen

--- project/front/src/forward-trading/views/FamilyOnboardingView.jsx
+++ project/front/src/forward-trading/views/FamilyOnboardingView.jsx
@@ -2,7 +2,6 @@ import { FamilyOnboardingForm } from "../components/FamilyOnboardingForm";
 
 export const FamilyOnboardingView = ({ onStepComplete }) => {
   const handleComplete = () => {
-    // Cette fonction sera appelée dès que le hook de mutation TanStack Query réussira
     if (onStepComplete) {
       onStepComplete();
     }
