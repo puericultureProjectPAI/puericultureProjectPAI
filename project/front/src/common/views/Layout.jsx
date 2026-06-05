4cd1a6db7effc6f3d18e0aee4d12d2894e3c0a62
Matéo Da Costa <mateodacosta21@gmail.com>
2026-06-05 17:52:23 +0200

    refactor: implement fixed-height layout with scrollable main content area

--- project/front/src/common/views/Layout.jsx
+++ project/front/src/common/views/Layout.jsx
@@ -5,13 +5,13 @@ import Navbar from "./NavBar";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-white">
+    <div className="flex h-[100dvh] flex-col overflow-hidden bg-white">
       {/* Persistant on every page */}
       <InstallPWA />
 
       <Header />
 
-      <main className="flex-grow">
+      <main className="flex-1 overflow-y-auto">
         {/* routes contents will be integrated here*/}
         <Outlet />
       </main>
