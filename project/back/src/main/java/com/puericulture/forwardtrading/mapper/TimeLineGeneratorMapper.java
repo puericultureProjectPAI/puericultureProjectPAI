5b7b77c497fb729ef5f3d4b0c11e77a44a938a2e
Xavier Moyon <moyonxavier@gmail.com>
2026-06-07 01:49:06 +0200

    feat(timeline generation): add timeline generation while creating one child

--- project/back/src/main/java/com/puericulture/forwardtrading/mapper/TimeLineGeneratorMapper.java
+++ project/back/src/main/java/com/puericulture/forwardtrading/mapper/TimeLineGeneratorMapper.java
@@ -2,6 +2,7 @@ package com.puericulture.forwardtrading.mapper;
 
 import com.puericulture.forwardtrading.dto.OnBoardingDto.ChildDto;
 import com.puericulture.forwardtrading.dto.TimeLineGeneratorCreateDto;
+import com.puericulture.forwardtrading.dto.children.CreateChildren;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,4 +11,8 @@ public abstract class TimeLineGeneratorMapper {
 
     @Mapping(source = "birthDate", target = "birthDate")
     public abstract TimeLineGeneratorCreateDto toTimeLineGeneratorCreateDto(ChildDto childDto);
+
+    @Mapping(source = "dpa", target = "birthDate")
+    public abstract TimeLineGeneratorCreateDto toTimeLineGeneratorCreateDto(
+            CreateChildren children);
 }
--- project/back/src/main/java/com/puericulture/forwardtrading/service/ChildrenService.java
+++ project/back/src/main/java/com/puericulture/forwardtrading/service/ChildrenService.java
@@ -3,8 +3,13 @@ package com.puericulture.forwardtrading.service;
 import com.puericulture.common.repository.PersonRepository;
 import com.puericulture.forwardtrading.dto.children.CreateChildren;
 import com.puericulture.forwardtrading.entity.ChildrenEntity;
+import com.puericulture.forwardtrading.entity.Timelines;
 import com.puericulture.forwardtrading.mapper.ChildrenMapper;
+import com.puericulture.forwardtrading.mapper.TimeLineGeneratorMapper;
 import com.puericulture.forwardtrading.repository.ChildrenRepository;
+import com.puericulture.forwardtrading.repository.TimelineEventRepository;
+import com.puericulture.forwardtrading.repository.TimelineRepository;
+import com.puericulture.forwardtrading.utils.timeline.generator.TimelineGenerator;
 import jakarta.transaction.Transactional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +21,20 @@ public class ChildrenService {
     private final ChildrenRepository childrenRepository;
     private final ChildrenMapper childrenMapper;
     private final PersonRepository personRepository;
+    private final TimelineGenerator timelineGenerator;
+    private final TimeLineGeneratorMapper timeLineGeneratorMapper;
+    private final TimelineRepository timelineRepository;
+    private final TimelineEventRepository timelineEventRepository;
 
     @Transactional
     public void createChildren(CreateChildren children, String userId) {
         ChildrenEntity childrenEntity = childrenMapper.toChildrenEntity(children);
         childrenEntity.setPerson(personRepository.getReferenceById(UUID.fromString(userId)));
         childrenRepository.save(childrenEntity);
+        Timelines timeline =
+                timelineGenerator.generateTimeline(
+                        timeLineGeneratorMapper.toTimeLineGeneratorCreateDto(children));
+        timeline.setChildren(childrenEntity);
+        Timelines savedTimeline = timelineRepository.save(timeline);
     }
 }
