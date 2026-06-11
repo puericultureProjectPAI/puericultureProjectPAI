184bdab73d63bac9521c429ae43ac2cad5a39a71
Xavier Moyon <moyonxavier@gmail.com>
2026-06-11 19:54:35 +0200

    fix(on-boarding): remove not null constraint on field dueDate

--- project/back/src/main/java/com/puericulture/forwardtrading/repository/ChildrenRepository.java
+++ project/back/src/main/java/com/puericulture/forwardtrading/repository/ChildrenRepository.java
@@ -1,6 +1,10 @@
 package com.puericulture.forwardtrading.repository;
 
 import com.puericulture.forwardtrading.entity.ChildrenEntity;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ChildrenRepository extends CrudRepository<ChildrenEntity, Integer> {}
+public interface ChildrenRepository extends CrudRepository<ChildrenEntity, Integer> {
+    List<ChildrenEntity> findAllByPersonIdAndNameNull(UUID personId);
+}
