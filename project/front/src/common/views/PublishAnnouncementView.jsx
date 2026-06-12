c4b494a01e987930f667ed80c08830a0adf4c0c0
Melissa <melissa.atmani.etu>
2026-06-13 00:01:51 +0200

    Redirection des annonces

--- project/front/src/common/views/PublishAnnouncementView.jsx
+++ project/front/src/common/views/PublishAnnouncementView.jsx
@@ -1,14 +1,23 @@
 import PublishAnnouncementForm from "../components/form/productCreation/PublishAnnouncementForm.jsx";
 import useTroc from "../../troc/hooks/useTroc.js";
+import useSecondHand from "../../second-hand/hooks/useSecondHand.js";
 
 export default function PublishAnnouncementView() {
-  const { error, publishTroc, success } = useTroc();
+  const { error: trocError, publishTroc, success: trocSuccess } = useTroc();
+  const { error: secondHandError, publishSecondHand, success: secondHandSuccess } = useSecondHand();
+
+  const handleSubmit = (mode, payload) => {
+    if (mode === "SECOND_HAND") {
+      return publishSecondHand(payload);
+    }
+    return publishTroc(payload);
+  };
 
   return (
     <PublishAnnouncementForm
-      error={error}
-      onSubmit={(_mode, payload) => publishTroc(payload)}
-      success={success}
+      error={trocError || secondHandError}
+      onSubmit={handleSubmit}
+      success={trocSuccess || secondHandSuccess}
     />
   );
 }
